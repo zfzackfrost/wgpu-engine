f6b5763462f0ed0b163bded2f167e5d07c840b37
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-08-29 17:06:52 -0400

    perf(app): inline functions: `app` `set_app`, `app_client_as`

--- src/app/current.rs
+++ src/app/current.rs
@@ -3,17 +3,20 @@ use std::sync::{Arc, OnceLock};
 use super::{AppClient, SharedApp};
 
 static APP: OnceLock<SharedApp> = OnceLock::new();
+#[inline]
 pub fn app() -> SharedApp {
     let Some(app) = APP.get() else {
         panic!("No current app!");
     };
     app.clone()
 }
+#[inline]
 pub(crate) fn set_app(app: SharedApp) {
     if APP.set(app).is_err() {
         panic!("An app is already running!");
     }
 }
+#[inline]
 pub fn app_client_as<C: AppClient>() -> Option<Arc<C>> {
     app().client().downcast_arc::<C>().ok()
 }
